8e0983a65a9a9a0cfea8e9e9dad3199430553e97
ozumoo <ozumoo@hotmail.com>
2020-04-16 01:31:19 +0200

    Design the Timeline of twitter using tailwindcss

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,17 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img 
+          src="https://i.pravatar.cc/50" 
+          alt=""
+          class="rounded-full mr-2" 
+        >
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        </p>
+    </div>
+</div>
