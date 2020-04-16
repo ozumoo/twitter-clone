0ce29382e89339a6d5791128e83fc2a5917165c3
ozumoo <ozumoo@hotmail.com>
2020-04-16 23:26:03 +0200

    Make the timeline dynamic

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,17 +1,16 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
         <img 
-          src="https://i.pravatar.cc/50" 
+          src="{{ $tweet->user->avatar }}" 
           alt=""
           class="rounded-full mr-2" 
         >
     </div>
     <div>
-        <h5 class="font-bold mb-4">John Doe</h5>
+        <h5 class="font-bold mb-4">{{ $tweet->user->name }}</h5>
 
         <p class="text-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          {{ $tweet->body }}
         </p>
     </div>
 </div>
